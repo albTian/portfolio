1f5020b9a211e0f0f40e7753231e620a096efe2e
Albert Tian <atian@berkeley.edu>
2023-05-25 15:10:07 -0400

    misc: removed export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
