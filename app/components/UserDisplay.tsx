f97ce62e674833ee01733b4e5a2f9e56137d777b
Albert Tian <atian@berkeley.edu>
2023-05-25 20:16:38 -0400

    misc: next/image

--- app/components/UserDisplay.tsx
+++ app/components/UserDisplay.tsx
@@ -11,20 +11,25 @@ interface Session {
   user?: User;
   expires?: string;
 }
-
 export const UserDisplay = () => {
   const { status, data: session } = useSession();
 
-  
   if (status === "loading") return <p>Loading...</p>;
-  
+
   if (status === "authenticated") {
     const { user, expires } = session as Session;
     return (
       <div>
         <h2>Welcome, {user?.name}</h2>
         <h3>Email: {user?.email}</h3>
-        <Image src={user?.image || ""} alt="User avatar" />
+        {user?.image && 
+          <Image 
+            src={user?.image || ""} 
+            alt="User avatar" 
+            width={50}  // Replace with your desired width
+            height={50} // Replace with your desired height
+          />
+        }
         <h4>Session Expires: {expires}</h4>
       </div>
     );
